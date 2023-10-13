5f30b5b8825eb4552f0360d8d032b6476258042b
Alan Prado <alangonzalezprado@gmail.com>
2023-10-12 23:07:39 -0600

    mobile-styles-p2

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  showMobileMenu: boolean = false;
+
+  showMenu() {
+    this.showMobileMenu = !this.showMobileMenu;
+  }
 
 }
